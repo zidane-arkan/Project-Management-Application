8a0e7adc09a2ddc2fe8ff3214639f067b5af1175
Zidane Arkan <zidark897@gmail.com>
2024-08-24 19:06:48 +0700

    Add New project components and make input / text area reusable

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import ProjectSidebar from "./components/ProjectSidebar";
-
+import NewProject from "./components/NewProject";
 function App() {
   return (
     <main className="min-h-screen flex gap-8 my-8">
       <ProjectSidebar />
+      <NewProject />
     </main>
   );
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Input({ label, textarea, ...props }) {
+  return (
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textarea ? (
+        <textarea
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+          {...props}
+        />
+      ) : (
+        <input
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+          {...props}
+        />
+      )}
+    </p>
+  );
+}
