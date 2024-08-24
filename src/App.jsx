6cd4f9ab70964c688e013929d55bb8eb37596f89
Zidane Arkan <zidark897@gmail.com>
2024-08-24 18:48:11 +0700

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
   return (
-    <main className="min-h-screen flex gap-4 my-8">
+    <main className="min-h-screen flex gap-8 my-8">
       <ProjectSidebar />
     </main>
   );
--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ProjectSidebar() {
   return (
-    <aside className="w-1/3 h-full px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
