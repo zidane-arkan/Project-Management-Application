a2bdf677bed1b94078a55a5596fcf798acb53254
Zidane Arkan <zidark897@gmail.com>
2024-08-24 18:32:55 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProjectSidebar from "./components/ProjectSidebar";
+
+function App() {
+  return (
+    <main className="min-h-screen flex gap-4 my-8">
+      <ProjectSidebar />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function ProjectSidebar() {
+  return (
+    <aside className="w-1/3 h-full px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        Your Projects
+      </h2>
+      <div>
+        <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          + Add Project
+        </button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
