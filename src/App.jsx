a223ab5e1fe96fb2bb0cc9fb543b32a0740b2744
Zidane Arkan <zidark897@gmail.com>
2024-08-25 17:12:53 +0700

    Make SelectedProject visible, when project selected via sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 import ProjectSidebar from "./components/ProjectSidebar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
+import SelectedProject from "./components/SelectedProject";
+// const data = {
+//   title: titleInput.current.value,
+//   description: descInput.current.value,
+//   dueDate: dateInput.current.value,
+// };
+
 function App() {
   const [selectedProject, setSelectedProject] = useState({
     // Undefined = Belum ada project yang dipilih, null = Project sedang ditambahkan tapi belum disimpan, idProject (example = 1) = Project sudah dibuat dan disimpan
@@ -58,11 +65,21 @@ function App() {
       />
     );
   }
+
+  // If selected project is has id (not undefined or null)
+  if (selectedProject.selectedProjectId) {
+    const projectSelected = selectedProject.projects.find(
+      (project) => project.id === selectedProject.selectedProjectId
+    );
+    // console.log(projectSelected);
+    content = <SelectedProject project={projectSelected} />;
+  }
   return (
     <main className="min-h-screen flex gap-8 my-8">
       <ProjectSidebar
         projects={selectedProject.projects}
         onShowNewProject={handleShowNewProject}
+        onSelectedProject={handleSelectedProject}
       />
       {content}
     </main>
--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Button from "./Button";
 
-export default function ProjectSidebar({ projects, onShowNewProject }) {
+export default function ProjectSidebar({
+  projects,
+  onShowNewProject,
+  onSelectedProject,
+}) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
@@ -13,7 +17,10 @@ export default function ProjectSidebar({ projects, onShowNewProject }) {
       <ul className="mt-8">
         {projects.map((project) => (
           <li key={project.id}>
-            <button className="w-full text-left px-2 py-1 rounded-md my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+            <button
+              onClick={() => onSelectedProject(project.id)}
+              className="w-full text-left px-2 py-1 rounded-md my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800"
+            >
               {project.title}
             </button>
           </li>
